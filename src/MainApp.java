0b9aa71a4d593f25c0d7039471af7a4e667e3107
alex <strim0125@yandex.ru>
2021-11-19 16:40:33 +0300

    Initial commit

---
+++ src/MainApp.java
@@ -0,0 +1,8 @@
+public class MainApp {
+    public static void main(String[] args) {
+        test1();
+    }
+    public static void test1() {
+        System.out.println("Hellow Wolrd !!!");
+    }
+}
