703dcb37765721d12b43466f0523e3d791f81620
alex <strim0125@yandex.ru>
2021-11-20 14:04:33 +0300

    4.1

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,7 +3,9 @@ public class MainApp {
         fruts();
         checkSumSign(10, -130);
         printColor(80);
+        compareNumbers(10, 230);
     }
+
     public static void fruts() {
         System.out.println("Orange");
         System.out.println("Banana");
@@ -19,12 +21,21 @@ public class MainApp {
         };
     }
 
-    public static void printColor(int value){
-        if(value <= 0){
+    public static void printColor(int value) {
+        if (value <= 0) {
             System.out.println("Красный");
-        } else if ( value > 0 || value <= 100){
+        } else if (value > 0 || value <= 100) {
             System.out.println("Жёлтый");
         } else {
             System.out.println("Зелёный");
-    };
+        };
+    }
+
+        public static void compareNumbers(int a, int b){
+            if( a >= b ){
+                System.out.println(" a >= b");
+            } else {
+                System.out.println(" a < b");
+            }
+        };
 }
