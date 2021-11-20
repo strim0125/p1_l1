6b724a9aac277f1685ed4124b0753af0617952e6
alex <strim0125@yandex.ru>
2021-11-20 13:15:52 +0300

    2

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,10 +1,21 @@
 public class MainApp {
     public static void main(String[] args) {
         fruts();
+        checkSumSign(10, -130);
     }
     public static void fruts() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
+    public static void checkSumSign(int a, int b){
+        int c = a + b;
+        if (c < 0 ) {
+            System.out.println("Отрицательная величина");}
+        else {
+            System.out.println("Положительная величина");
+        };
+    }
+
 }
